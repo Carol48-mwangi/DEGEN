ec801f171b1392881e07a3b70f6a69d10945e948
Carol48-mwangi <mwangicaroline517@gmail.com>
2024-07-26 13:14:40 +0100

    Update Degen.sol

--- Degen.sol
+++ Degen.sol
@@ -5,15 +5,15 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
+
 contract DegenToken is ERC20, ERC20Burnable, Ownable {
-   
     enum ItemType { Laptop, GamingMouse, Keyboard, Headset, ExternalHardDrive }
     
     event ItemRedeemed(address indexed player, ItemType itemType, uint quantity);
     
     mapping(address => mapping(ItemType => uint)) public itemInventory;
 
-    constructor() ERC20("Degen Gaming Token", "DGT") Ownable(msg.sender) {
+    constructor() ERC20("Degen Gaming Token", "DGT") Ownable(msg.sender)  {
         _mint(msg.sender, 1000 * 10 ** decimals()); // Initial supply for liquidity
     }
 
@@ -21,8 +21,23 @@ contract DegenToken is ERC20, ERC20Burnable, Ownable {
         _mint(to, amount);
     }
 
-    
-    function redeemTokens(ItemType itemType, uint quantity, uint tokenCost) public {
+    function redeemTokens(ItemType itemType, uint quantity) public {
+        uint tokenCost;
+        
+        if (itemType == ItemType.Laptop) {
+            tokenCost = 10 * 10 ** decimals();
+        } else if (itemType == ItemType.GamingMouse) {
+            tokenCost = 5 * 10 ** decimals();
+        } else if (itemType == ItemType.Keyboard) {
+            tokenCost = 7 * 10 ** decimals();
+        } else if (itemType == ItemType.Headset) {
+            tokenCost = 6 * 10 ** decimals();
+        } else if (itemType == ItemType.ExternalHardDrive) {
+            tokenCost = 8 * 10 ** decimals();
+        } else {
+            revert("Invalid item type");
+        }
+
         uint totalCost = tokenCost * quantity;
         require(balanceOf(msg.sender) >= totalCost, "Insufficient balance to redeem items");
         _burn(msg.sender, totalCost);       
@@ -33,7 +48,7 @@ contract DegenToken is ERC20, ERC20Burnable, Ownable {
     function checkBalance(address account) public view returns (uint256) {
         return balanceOf(account);
     }
-   
+
     function getItemQuantity(address player, ItemType itemType) public view returns (uint) {
         return itemInventory[player][itemType];
     }
